b91a389b31f380cb8690a0fd277e01bed6d8f66f
jha269 <jha269@uclive.ac.nz>
2018-08-08 14:09:26 +1200

    added a new test Array

--- TractFitt/src/main/java/seng202/groupNine/App.java
+++ TractFitt/src/main/java/seng202/groupNine/App.java
@@ -2,19 +2,26 @@ package seng202.groupNine;
 
 import java.util.ArrayList;
 /**
- * Hello world!
+ * Hello world of Arrays!
  *
  */
 public class App {
+    private int testArray[] = {0, 1, 2, 3, 4, 5};
+    public int[] getTestArray() {
+        return testArray;
+    }
+
+    public void setTestArray(int[] testArray) {
+        this.testArray = testArray;
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        ArrayList<Integer> TestArray = new ArrayList<Integer>();
-        TestArray.add(1);
-        TestArray.add(2);
-        TestArray.add(3);
-        TestArray.add(4);
-        TestArray.add(5);
-        System.out.println(TestArray);
+        App app = new App();
+        int x[] = app.getTestArray();
+        System.out.println(x[2]);
     }
 
 }
